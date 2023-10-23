c95b96036bdbe15915664200b92540536d968bef
Cole Smith <colesmith5400@gmail.com>
2023-10-22 21:00:34 -0400

    lesson 5

---
+++ week2_js/js5/js/main.js
@@ -0,0 +1,14 @@
+console.log(Math.PI);
+
+console.log(Math.trunc(Math.PI));
+console.log(Math.round(3.5));
+console.log(Math.ceil(Math.PI));
+
+console.log(Math.pow(2, 8));
+
+console.log(Math.min(Math.PI, 9, -2, 3));
+console.log(Math.max(Math.PI, 9, -2, 3));
+
+console.log(Math.random());
+
+console.log(parseInt(Math.random() * 10) + 1);
