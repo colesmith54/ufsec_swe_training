e0df512674106424df002e71f1fe9bb293256887
Cole Smith <colesmith5400@gmail.com>
2023-10-26 22:15:58 -0400

    break time

--- week3_node/js24/js/main.js
+++ week3_node/js24/js/main.js
@@ -1,37 +1,3 @@
-// window.alert("ok!");
-// alert("ok!"); // window is implied
+// modules automatically use strict mode
 
-// console.log(window.location);
-// console.log(location); // window is implied
-
-const myArray = ["eat", "sleep", "code"];
-const myObj = {
-  name: "cole",
-  hobbies: ["eat", "sleep", "code"],
-  logName: function () {
-    console.log(this.name);
-  },
-};
-
-// local storage is persistent
-// session storage is not
-
-sessionStorage.setItem("mySessionStore", JSON.stringify(myObj));
-const mySessionData = JSON.parse(sessionStorage.getItem("mySessionStore"));
-
-// local and session storage only store strings
-
-console.log(mySessionData);
-
-sessionStorage.setItem("mySessionStoreA", JSON.stringify(myArray));
-const mySessionArray = JSON.parse(sessionStorage.getItem("mySessionStoreA"));
-
-console.log(mySessionArray);
-
-sessionStorage.removeItem("mySessionStore");
-
-console.log(JSON.parse(sessionStorage.getItem("mySessionStore")));
-
-sessionStorage.clear();
-
-console.log(sessionStorage);
+// 5:43:17
