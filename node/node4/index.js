02c96f5f24a76782dfc2fe2132a5c4e6921b045b
Cole Smith <colesmith5400@gmail.com>
2023-12-07 23:25:35 -0500

    node lesson 4 done

---
+++ node/node4/index.js
@@ -0,0 +1,15 @@
+const logEvents = require("./logEvents");
+
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+
+// init event emitter
+const myEmitter = new MyEmitter();
+
+// add listener
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+    myEmitter.emit("log", "Log event emitted");
+}, 2000);
