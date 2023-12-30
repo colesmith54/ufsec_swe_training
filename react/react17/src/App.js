87f66437d7fc0cb2ee108ef6ab61383ae6a00daa
Cole Smith <colesmith5400@gmail.com>
2023-12-29 20:57:35 -0500

    progress react lesson 17

---
+++ react/react17/src/App.js
@@ -0,0 +1,70 @@
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function App() {
+    const [posts, setPosts] = useState([
+        {
+            id: 1,
+            title: "My First Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 2,
+            title: "My 2nd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 3,
+            title: "My 3rd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 4,
+            title: "My Fourth Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+    ]);
+    const [search, setSearch] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    const history = useHistory();
+
+    const handleDelete = (id) => {
+        const postsList = posts.filter((post) => post.id !== id);
+        setPosts(postsList);
+        history.push("/");
+    };
+
+    return (
+        <div className="App">
+            <Header title="React JS Blog" />
+            <Nav search={search} setSearch={setSearch} />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/post" element={<NewPost />} />
+                <Route
+                    path="/post/:id"
+                    element={
+                        <PostPage posts={posts} handleDelete={handleDelete} />
+                    }
+                />
+                <Route path="/about" element={<About />} />
+                <Route path="*" element={<Missing />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
